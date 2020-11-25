54cbf85e45224c0ef39e45ea60b39f892f222b03
Evgeniy Solodkiy <asynker29@gmai.com>
2020-11-25 15:03:40 +0300

    Added Anykernel3

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,55 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Englezos Kernel for Redmi Note 4(X)
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=mido
+device.name2=redmi note 4
+device.name3=Redmi Note 4
+device.name4=Redmi Note 4x
+supported.versions=8.1.0 - 10
+supported.patchlevels=2019-01 -
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+## Treble Check
+is_treble=$(file_getprop /system/build.prop "ro.treble.enabled");
+if [ ! "$is_treble" -o "$is_treble" == "false" ]; then
+  ui_print " ";
+  ui_print "Englezos Kernel only supports Treble ROMS!";
+  exit 1;
+fi;
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+# init.rc
+insert_line init.rc "import /init.englezos.rc" after "import /init.trace.rc" "import /init.englezos.rc";
+# end ramdisk changes
+
+write_boot;
+## end install
+
